2983aa293b424e05197a2899b3e3e0584cc81831
Lydia <lshim1720@gmail.com>
2021-03-27 09:54:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,107 +19,107 @@ const api = require('./utils/api');
 const markdown = require('./utils/generateMarkdown');
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
-const questions = [
-    {
-        type: "input",
-        message: "What is your GitHub user name?",
-        name: "username"
-    },
-    {
-        type: "input",
-        message: "What is your email address?",
-        name: "email"
-    },
-    {
-        type: "input",
-        message: "What is your project's title?",
-        name: "title"
-    },
-    {
-        type: "input",
-        message: "Please write a short description of your project.",
-        name: "description"
-    },
-    {
-        type: "list",
-        message: "What license should your project have?",
-        name: "license",
-        choices: [
-            "MIT",
-            "Unlicense",
-            "Apache 2.0",
-            "GNU v3",
-            "BSD 3-Clause",
-            "Mozilla Public License 2.0"
-        ]
-    },
-    {
-        type: "input",
-        message: "What command should be run to install dependencies?",
-        name: "installation",
-        default: "npm i"
-    },
-    {
-        type: "input",
-        message: "What command should be run to run tests?",
-        name: "tests",
-        default: "npm run test"
-    },
-    {
-        type: "input",
-        message: "What does the user need to know about using the repository?",
-        name: "usage"
-    },
-    {
-        type: "input",
-        message: "What does the user need to know about contributing to the repository?",
-        name: "contribute"
-    }
-];
-
 // const questions = [
 //     {
 //         type: "input",
-//         message: "Enter your Github username: ",
+//         message: "What is your GitHub user name?",
 //         name: "username"
 //     },
 //     {
 //         type: "input",
-//         message: "Enter your project title: ",
+//         message: "What is your email address?",
+//         name: "email"
+//     },
+//     {
+//         type: "input",
+//         message: "What is your project's title?",
 //         name: "title"
 //     },
 //     {
 //         type: "input",
-//         message: "Enter a short project description: ",
+//         message: "Enter a short description of your project.",
 //         name: "description"
 //     },
 //     {
-//         type: "input",
-//         message: "Enter the installation process: ",
-//         name: "installation"
+//         type: "list",
+//         message: "What license should your project have?",
+//         name: "license",
+//         choices: [
+//             "MIT",
+//             "Unlicense",
+//             "Apache 2.0",
+//             "GNU v3",
+//             "BSD 3-Clause",
+//             "Mozilla Public License 2.0"
+//         ]
 //     },
 //     {
 //         type: "input",
-//         message: "Enter the usage of the project: ",
+//         message: "Enter the installation process",
+//         name: "installation",
+//         default: "npm"
+//     },
+//     {
+//         type: "input",
+//         message: "What command should be run to run tests?",
+//         name: "tests",
+//         default: "npm run test"
+//     },
+//     {
+//         type: "input",
+//         message: "What does the user need to know about using the repository?",
 //         name: "usage"
-
 //     },
 //     {
 //         type: "input",
-//         message: "Enter licenses used for the project: ",
-//         name: "licenses"
-//     },
-//     {
-//         type: "input",
-//         message: "Enter contributing information: ",
+//         message: "What does the user need to know about contributing to the repository?",
 //         name: "contribute"
-
-//     }, {
-//         type: "input",
-//         message: "Enter any tests you are running for your project: ",
-//         name: "tests"
 //     }
 // ];
+
+const questions = [
+    {
+        type: "input",
+        message: "Enter your Github username: ",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "Enter your project title: ",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter a short project description: ",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Enter the installation process: ",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Enter the usage of the project: ",
+        name: "usage"
+
+    },
+    {
+        type: "input",
+        message: "Enter licenses used for the project: ",
+        name: "licenses"
+    },
+    {
+        type: "input",
+        message: "Enter contributing information: ",
+        name: "contribute"
+
+    }, {
+        type: "input",
+        message: "Enter any tests you are running for your project: ",
+        name: "tests"
+    }
+];
 
 // function to prompt user - returns answers object
 const promptUser = () => {
